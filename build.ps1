162c1518e1dcafbe3a7697bedc6a159e1d55acb1
YuQi <qiyu_sjtu@163.com>
2022-05-06 22:31:03 +0800

    update build

--- build.ps1
+++ build.ps1
@@ -1,1 +1,1 @@
-pyinstaller --noconfirm --add-data "DataGrabber/img/*.*;." -Fw main.py 
+pyinstaller --noconfirm --add-data "DataGrabber/img/*.*;."  --upx-dir=D:\YuQi\Desktop\upx-3.96-win64 -F -Dw main.py 
